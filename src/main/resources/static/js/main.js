2db4d95c8e988cb6612df84396424c50a26ca076
haruhi sasaki <onedraft82@yahoo.co.jp>
2020-03-26 16:04:56 +0900

    first commit

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,14 @@
+
+function checkradioA() {
+   document.getElementById('kensaku').style.display = 'inline';
+   document.getElementById('shinki').style.display = 'none';
+}
+
+function checkradioB() {
+	   document.getElementById('kensaku').style.display = 'none';
+	   document.getElementById('shinki').style.display = 'inline';
+	}
+
+
+
+
