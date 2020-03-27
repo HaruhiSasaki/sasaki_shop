fc60ca12fb2778de1965ee2a56e95739756062a9
haruhi sasaki <onedraft82@yahoo.co.jp>
2020-03-27 12:12:05 +0900

    product_masterのモデル作成

---
+++ src/main/java/com/example/demo/Product_master.java
@@ -0,0 +1,96 @@
+package com.example.demo;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Product_master {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private String product; //商品コード
+
+  private String shohinname; //商品名
+
+  private String torokusya; //登録者
+
+  private String torokupg; //登録プログラム
+
+  private Integer torokudate; //登録日　timestanpとはなんだったのか？？？橋本さん確認
+
+  private String kosinsya; //更新者
+
+  private Integer kosindate; //更新日
+
+  private Integer vershion; //バージョン
+
+
+  public String getProduct() {
+    return product;
+  }
+
+  public void setId(String product) {
+    this.product = product;
+  }
+
+public String getShohinname() {
+	return shohinname;
+}
+
+public void setShohinname(String shohinname) {
+	this.shohinname = shohinname;
+}
+
+public String getTorokusya() {
+	return torokusya;
+}
+
+public void setTorokusya(String torokusya) {
+	this.torokusya = torokusya;
+}
+
+public String getTorokupg() {
+	return torokupg;
+}
+
+public void setTorokupg(String torokupg) {
+	this.torokupg = torokupg;
+}
+
+public Integer getTorokudate() {
+	return torokudate;
+}
+
+public void setTorokudate(Integer torokudate) {
+	this.torokudate = torokudate;
+}
+
+public String getKosinsya() {
+	return kosinsya;
+}
+
+public void setKosinsya(String kosinsya) {
+	this.kosinsya = kosinsya;
+}
+
+public Integer getKosindate() {
+	return kosindate;
+}
+
+public void setKosindate(Integer kosindate) {
+	this.kosindate = kosindate;
+}
+
+public Integer getVershion() {
+	return vershion;
+}
+
+public void setVershion(Integer vershion) {
+	this.vershion = vershion;
+}
+
+
+
+}
