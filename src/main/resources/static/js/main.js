5b4f8b2778672d657fbb7d0509de0dabaae05bd3
haruhi sasaki <onedraft82@yahoo.co.jp>
2020-04-01 19:16:17 +0900

    jqueryに変更

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,14 +1,33 @@
+$(function() {
+	$('#shinki').hide();
+	$('#kekka').hide();
 
-function checkradioA() {
-   document.getElementById('kensaku').style.display = 'inline';
-   document.getElementById('shinki').style.display = 'none';
-}
+//ーーーーーーーラジオボタン操作-------------//
+    $('#a').on('click', () => {
+        $('#kensaku').show();
+    });
 
-function checkradioB() {
-	   document.getElementById('kensaku').style.display = 'none';
-	   document.getElementById('shinki').style.display = 'inline';
-	}
+    $('#a').on('click', () => {
+        $('#shinki').hide();
+    });
+
+    $('#b').on('click', () => {
+        $('#shinki').show();
+    });
+
+    $('#b').on('click', () => {
+        $('#kensaku').hide();
+    });
+
+//--------------------------------------//
+
+//検索ボタン
+    $('#MakerSearch').on('click', () => {
+        $('#kekka').show();
+    });
 
 
+
+ });
 
 
